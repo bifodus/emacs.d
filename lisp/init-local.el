8bf2da6782906e6fe80a97e0d564c0f27e42dbb0
bifodus <bifodus@gmail.com>
2018-04-25 14:45:56 -0700

    Add keybinding for counsel-yank-pop

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,6 +3,7 @@
  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
+(global-set-key (kbd "M-y") 'counsel-yank-pop)
 (global-set-key (kbd "M-o") 'ace-window)
 (setq debug-on-error nil)
 
