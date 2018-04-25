e2355c996003bfe76c3c91f7126f90b29426a59e
bifodus <bifodus@gmail.com>
2018-04-24 23:25:21 -0700

    Add init-local

---
+++ lisp/init-local.el
@@ -0,0 +1,9 @@
+(custom-set-variables
+ '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+ '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
+(global-set-key (kbd "M-o") 'ace-window)
+(setq debug-on-error nil)
+
+(provide 'init-local)
